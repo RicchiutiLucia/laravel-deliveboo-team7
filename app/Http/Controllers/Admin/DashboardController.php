9738f5163d1c15e77e3a538059e6c603c9fd74f8
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-06-16 17:07:18 +0200

    dashboard controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -10,7 +10,17 @@ use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
 {
     public function index(){
-        $restaurants = Restaurant::findOrFail(Auth::id());
-        return view('admin.dashboard', compact('restaurants'));
+        if (Auth::user()->restaurant) {
+            // ('hai il ristorante');
+
+            $restaurant = Restaurant::where('user_id', Auth::id())->first();
+            // ($restaurant);
+           
+            return view('admin.dashboard', compact('restaurant'));
+        } else {
+            // ('non hai il ristorante');
+            return redirect()->route('admin.restaurants.create');
+        }
     }
-}
+    }
+
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -42,7 +42,7 @@
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{  route('admin.restaurants.store')  }}">{{__('Dashboard')}}</a>
+                        <a class="dropdown-item" href="{{  route('admin.restaurants.create')  }}">{{__('Dashboard')}}</a>
                         <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
                         <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
