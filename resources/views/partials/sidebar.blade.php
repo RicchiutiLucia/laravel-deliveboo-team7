8110fe99ba5251b27faaf71ce3e447541a4566ad
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-06-20 10:31:31 +0200

    aggiunta orders alla sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,7 +1,8 @@
 <div class="d-flex flex-column py-3 bg-body-tertiary border my-5">
     <ul class="nav nav-pills flex-column">
         <li class="nav-item">
-            <a href="{{route('admin.dashboard')}}" class="nav-link {{Route::currentRouteName() == 'admin.dashboard'?'active':''}}" aria-current="page">
+            <a href="{{ route('admin.dashboard') }}"
+                class="nav-link {{ Route::currentRouteName() == 'admin.dashboard' ? 'active' : '' }}" aria-current="page">
                 <svg class="bi pe-none me-2" width="16" height="16">
                     <use xlink:href="#home"></use>
                 </svg>
@@ -10,7 +11,8 @@
         </li>
 
         <li class="nav-item">
-            <a href="{{route('admin.dishes.index')}}" class="nav-link @if (Route::currentRouteName() == 'admin.dishes.index') active @endif">
+            <a href="{{ route('admin.dishes.index') }}"
+                class="nav-link @if (Route::currentRouteName() == 'admin.dishes.index') active @endif">
                 <svg class="bi pe-none me-2" width="16" height="16">
                     <use xlink:href="#home"></use>
                 </svg>
@@ -18,13 +20,25 @@
             </a>
         </li>
         <li class="nav-item">
-            <a href="{{route('admin.dishes.create')}}" class="nav-link @if (Route::currentRouteName() == 'admin.dishes.create') active @endif">
+            <a href="{{ route('admin.dishes.create') }}"
+                class="nav-link @if (Route::currentRouteName() == 'admin.dishes.create') active @endif">
                 <svg class="bi pe-none me-2" width="16" height="16">
                     <use xlink:href="#home"></use>
                 </svg>
                 Crea nuovo piatto
             </a>
         </li>
+
+        <li class="nav-item">
+            <a href="{{ route('admin.orders.index') }}"
+                class="nav-link @if (Route::currentRouteName() == 'admin.orders.index') active @endif">
+                <svg class="bi pe-none me-2" width="16" height="16">
+                    <use xlink:href="#home"></use>
+                </svg>
+                Ordini
+            </a>
+        </li>
+
 
     </ul>
 </div>
