1195897d4f8dc615039e15ed6cc129a7dbfb150c
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-06-21 16:05:48 +0200

    pagina not found

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Pagina non trovata</title>
+</head>
+<body>
+
+    <div class="d-flex align-items-center justify-content-center vh-100">
+        <div class="text-center">
+            <h1 class="display-1 fw-bold">404</h1>
+            <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
+            <p class="lead">
+                La pagina ricercata non esiste!!
+              </p>
+            <a href="{{ route('admin.dashboard') }}" class="btn btn-primary">Torna alla Dashboard</a>
+        </div>
+    </div>
+    
+</body>
+</html>
