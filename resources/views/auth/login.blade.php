9b59c5f2e8678fc688f0c4e9037d6425e199179a
Andrea Fermo <andreafermo.sd@gmail.com>
2023-07-03 19:22:09 +0200

    nuovi dati

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="mb-4 row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">Indirro Email</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Indirizzo Email</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
