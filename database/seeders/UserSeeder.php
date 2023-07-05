bb7af91ee84a886f6e372cfe5aa6611b89486b66
David Dume <dumedavid11@gmail.com>
2023-07-05 15:03:48 +0200

    pulizia codice

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -27,16 +27,10 @@ class UserSeeder extends Seeder
             $newuser->name = $faker->firstName();
             $newuser->email = $faker->email();
             $newuser->password = bcrypt('gruppo7');
-            // $newuser->role = 'owner';
             $newuser->save();
         }
 
-        // $newuser = new User();
-        // $newuser->name = $faker->firstName();
-        // $newuser->email = 'admin@admin.com';
-        // $newuser->password = bcrypt('carlo');
-        // $newuser->role = 'admin';
-        // $newuser->save();
+
 
 
         DB::statement("SET foreign_key_checks = 1;");
